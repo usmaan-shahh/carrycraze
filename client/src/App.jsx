a9dc30efbfebe1fdc82fcf1194ba9cdb0299ca88
steins <steins007j@gmail.com>
2026-07-15 13:07:24 +0530

    Add blog routes and wrap app with HelmetProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost";
 
 const App = () => {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    // HelmetProvider lets each page set its own <title> and <meta> tags (SEO)
+    <HelmetProvider>
+      <Router>
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              {/* Blog listing page */}
+              <Route path="/blog" element={<Blog />} />
+              {/* Single blog post page (slug comes from blogData.js) */}
+              <Route path="/blog/:slug" element={<BlogPost />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </HelmetProvider>
   );
 };
 
