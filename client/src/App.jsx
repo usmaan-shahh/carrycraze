25c2146dafe88321bb1de96551dc8631a98c67a0
steins <steins007j@gmail.com>
2026-07-15 16:15:39 +0530

    App.jsx: remove HelmetProvider (no longer needed)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -11,26 +10,23 @@ import BlogPost from "./pages/BlogPost";
 
 const App = () => {
   return (
-    // HelmetProvider lets each page set its own <title> and <meta> tags (SEO)
-    <HelmetProvider>
-      <Router>
-        <div className="min-h-screen flex flex-col">
-          <Navbar />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              {/* Blog listing page */}
-              <Route path="/blog" element={<Blog />} />
-              {/* Single blog post page (slug comes from blogData.js) */}
-              <Route path="/blog/:slug" element={<BlogPost />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </HelmetProvider>
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            {/* Blog listing page */}
+            <Route path="/blog" element={<Blog />} />
+            {/* Single blog post page (slug comes from blogData.js) */}
+            <Route path="/blog/:slug" element={<BlogPost />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
